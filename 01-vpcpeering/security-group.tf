1882c114a09ef4fcfa15db8ea4edd17bf542af2d
THANIGAIVELAN <velan.techlab@gmail.com>
2025-04-05 20:36:07 +0800

    added vpc peering

---
+++ 01-vpcpeering/security-group.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "alb-sg" {
+  name        = "alb-sg"
+  description = "Allow TLS and http inbound traffic"
+  vpc_id      = "${aws_vpc.app-vpc.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
